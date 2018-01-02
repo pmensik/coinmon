e9849e60fb87b75969eb20c676a2c3196c684840
Petr Mensik <petr.mensik1@gmail.com>
2018-01-02 22:18:57 +0100

    Added custom changes for refreshing

--- src/index.js
+++ src/index.js
@@ -26,29 +26,30 @@ if (availableCurrencies.indexOf(convert) === -1) {
   return console.log('We cannot convert to your fiat currency.'.red)
 }
 const find = program.find
-const top = !isNaN(program.top) && +program.top > 0 ? +program.top : (find.length > 0 ? 1500 : 10)
+const top = 200 //!isNaN(program.top) && +program.top > 0 ? +program.top : (find.length > 0 ? 1500 : 10)
 const humanizeIsEnabled = program.humanize !== 'false'
-const table = new Table({
-  chars: {
-    'top': '-',
-    'top-mid': '-',
-    'top-left': '-',
-    'top-right': '-',
-    'bottom': '-',
-    'bottom-mid': '-',
-    'bottom-left': '-',
-    'bottom-right': '-',
-    'left': '║',
-    'left-mid': '-' ,
-    'mid': '-' ,
-    'mid-mid': '-',
-    'right': '║',
-    'right-mid': '-',
-    'middle': '│'
-  },
-  head: ['Rank', 'Coin', `Price (${convert})`, 'Change (24H)', 'Change (1H)', `Market Cap (${convert})`].map(title => title.yellow),
-  colWidths: [6, 14, 15, 15, 15, 20]
-})
+
+function createTable () {
+    new Table({
+        chars: {
+            'top': '-',
+            'top-mid': '-',
+            'top-left': '-',
+            'top-right': '-',
+            'bottom': '-',
+            'bottom-mid': '-',
+            'bottom-left': '-',
+            'bottom-right': '-',
+            'left': '║',
+            'left-mid': '-' ,
+            'mid': '-' ,
+            'mid-mid': '-',
+            'right': '║',
+            'right-mid': '-',
+            'middle': '│'
+        },
+        head: ['Rank', 'Coin', `Price (${convert})`, 'Change (1H)', 'Change (24H)',`Market Cap (${convert})`].map(title => title.yellow),
+        colWidths: [6, 14, 15, 15, 15, 20]})}
 
 cfonts.say('coinmon', {
   font: 'simple3d',
@@ -60,45 +61,78 @@ cfonts.say('coinmon', {
   space: true,
   maxLength: '0'
 })
+
+function makeSortable(percent) {
+    return parseFloat(percent.slice(0, -1));
+}
 const spinner = ora('Loading data').start()
 const sourceUrl = `https://api.coinmarketcap.com/v1/ticker/?limit=${top}&convert=${convert}`
-axios.get(sourceUrl)
-.then(function (response) {
-  spinner.stop()
-  response.data
-    .filter(record => {
-      if (find.length > 0) {
-        return find.some(keyword => record.symbol.toLowerCase() === keyword.toLowerCase())
-      }
-      return true
-    })
-    .map(record => {
-      const percentChange24h = record.percent_change_24h
-      const textChange24h = `${percentChange24h}%`
-      const change24h = percentChange24h? (percentChange24h > 0 ? textChange24h.green : textChange24h.red) : 'NA'
-      const percentChange1h = record.percent_change_1h
-      const textChange1h = `${percentChange1h}%`
-      const change1h = percentChange1h ? (percentChange1h > 0 ? textChange1h.green : textChange1h.red) : 'NA'
-      const marketCap = record[`market_cap_${convert}`.toLowerCase()]
-      const displayedMarketCap = humanizeIsEnabled ? humanize.compactInteger(marketCap, 3) : marketCap
-      return [
-        record.rank,
-        `${supportEmoji ? '💰  ' : ''}${record.symbol}`,
-        record[`price_${convert}`.toLowerCase()],
-        change24h,
-        change1h,
-        displayedMarketCap
-      ]
-    })
-    .forEach(record => table.push(record))
-  if (table.length === 0) {
-    console.log('We are not able to find coins matching your keywords'.red)
-  } else {
-    console.log(`Data source from coinmarketcap.com at ${new Date().toLocaleTimeString()}`)
-    console.log(table.toString())
-  }
-})
-.catch(function (error) {
-  spinner.stop()
-  console.error('Coinmon is not working now. Please try again later.'.red)
-})
+
+function drawData () {
+        table = new Table({
+        chars: {
+            'top': '-',
+            'top-mid': '-',
+            'top-left': '-',
+            'top-right': '-',
+            'bottom': '-',
+            'bottom-mid': '-',
+            'bottom-left': '-',
+            'bottom-right': '-',
+            'left': '║',
+            'left-mid': '-' ,
+            'mid': '-' ,
+            'mid-mid': '-',
+            'right': '║',
+            'right-mid': '-',
+            'middle': '│'
+        },
+        head: ['Rank', 'Coin', `Price (${convert})`, 'Change (1H)', 'Change (24H)',`Market Cap (${convert})`].map(title => title.yellow),
+        colWidths: [6, 14, 15, 15, 15, 20]})
+    axios.get(sourceUrl)
+        .then(function (response) {
+            spinner.stop()
+                response.data
+                .sort(function (a, b) {
+                    return (makeSortable(a.percent_change_1h) > makeSortable(b.percent_change_1h)) ? 1 : 
+                        ((makeSortable(b.percent_change_1h) > makeSortable(a.percent_change_1h)) ? -1 : 0);})
+            .reverse()
+            .slice(0, 30)
+            .filter(record => {
+                if (find.length > 0) {
+                    return find.some(keyword => record.symbol.toLowerCase() === keyword.toLowerCase())
+                }
+                return true
+            })
+            .map(record => {
+                const percentChange24h = record.percent_change_24h
+                    const textChange24h = `${percentChange24h}%`
+                    const change24h = percentChange24h? (percentChange24h > 0 ? textChange24h.green : textChange24h.red) : 'NA'
+                    const percentChange1h = record.percent_change_1h
+                    const textChange1h = `${percentChange1h}%`
+                    const change1h = percentChange1h ? (percentChange1h > 0 ? textChange1h.green : textChange1h.red) : 'NA'
+                    const marketCap = record[`market_cap_${convert}`.toLowerCase()]
+                    const displayedMarketCap = humanizeIsEnabled ? humanize.compactInteger(marketCap, 3) : marketCap
+                    return [
+                    record.rank,
+                    `${supportEmoji ? '💰  ' : ''}${record.symbol}`,
+                    record[`price_${convert}`.toLowerCase()],
+                    change1h,
+                    change24h,
+                    displayedMarketCap
+                    ]
+            })
+            .forEach(record => table.push(record))
+                if (table.length === 0) {
+                    console.log('We are not able to find coins matching your keywords'.red)
+                } else {
+                    console.log(`Data source from coinmarketcap.com at ${new Date().toLocaleTimeString()}`)
+                    console.log(table.toString())
+                }
+        })
+    .catch(function (error) {
+        spinner.stop()
+            console.error('Coinmon is not working now. Please try again later.'.red)
+    });
+}
+setInterval(drawData, 60000);
